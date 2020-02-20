eebbc623aff7ef1da7a23434b84d39f08af97428
bruno montebello <brunormontebello@hotmail.com>
2020-02-20 15:22:35 -0300

    Seeding

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,6 @@
 class Cocktail < ApplicationRecord
   validates :name, uniqueness: true
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
 end
-
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,4 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail
   belongs_to :ingredient
+  belongs_to :cocktail
 end
